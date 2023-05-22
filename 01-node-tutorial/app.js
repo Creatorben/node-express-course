57642890cdf94d99d4ff9029aa963ac77fe859aa
Beni <bkoller@locorum.ch>
2023-05-22 18:31:43 +0200

    test

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,1 +1,15 @@
-console.log('Welcome to Node Tutorial')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('Home Page');
+  }
+  if (req.url === '/about') {
+    res.end('About Page');
+  }
+  res.end('Error Page');
+});
+
+server.listen(5000, () => {
+  console.log('Server is listening on port 5000...');
+});
